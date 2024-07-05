a5967af0035e49cccdaf848b2c8e6beec7283df7
JamieHallett <jamie66.hallett@gmail.com>
2024-07-05 15:04:01 +0000

    Artillery and menu

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const degToRad = Math.PI/180
 const floorY = 440;
 let right = 0;
 let left = 0;
 let up = 0;
 let down = 0;
+let mouseX = 0;
+let mouseY = 0;
 const Square = {
   elem: document.getElementById("square"),
   X: 0,
@@ -14,11 +17,19 @@ const Square = {
   speed: 10,
   sizetospd: 0.2,
   size: 50,
-  grounded: false
+  grounded: false,
+  Cannon: {
+    angle: 0,
+    vel: 75,
+  },
 }
 let mode = false;
 let trail = false;
+let menu = false;
 let projectileID = 0;
+let artillery = [];
+
+//document.onmousemove = mousePosUpd;
 
 function squareleft(v) {
   Square.X -= v; //moves square left
@@ -40,6 +51,16 @@ function squaredown(v) {
   square.style.top = Square.Y - Square.size/2 + "px";
 }
 
+function objUp(obj, v) {
+  obj.Y -= v; //moves object up
+  obj.elem.style.top = obj.Y - obj.size/2 + "px";
+}
+function objDown(obj, v) {
+  obj.Y += v; //moves object down
+  obj.elem.style.top = obj.Y - obj.size/2 + "px";
+}
+
+
 const slider = document.getElementById("myRange");
 const output = document.getElementById("slidervalue");
 
@@ -54,6 +75,29 @@ slider.oninput = function() {
   Square.size = this.value;
   Square.speed = this.value * Square.sizetospd;
 }
+
+const slider2 = document.getElementById("cannonAngle");
+const output2 = document.getElementById("cannonAngValue");
+
+output2.innerHTML = slider2.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+  Square.Cannon.angle = this.value;
+}
+
+/* Set the width of the side navigation to 250px */
+ function openNav() {
+   menu = true
+   document.getElementById("mySidenav").style.width = "250px";
+ }
+
+ /* Set the width of the side navigation to 0 */
+ function closeNav() {
+   menu = false
+   document.getElementById("mySidenav").style.width = "0";
+ }
 
 // Add event listener on keydown
 document.addEventListener('keydown', (event) => {
@@ -98,6 +142,19 @@ document.addEventListener('keyup', (event) => {
   if (name.toLowerCase() == "b") {
     makeprojectile()
   }
+  if (name.toLowerCase() == "c") {
+    if (!menu) {openNav()}
+    else {closeNav()}
+  }
+  if (name.toLowerCase() == "e") {
+    squaretomouse();
+  }
+  if (name.toLowerCase() == "r") {
+    removprojecs();
+  }
+  if (name.toLowerCase() == "v") {
+    makeprojectile(true);
+  }
   
 }, false);
 
@@ -111,10 +168,11 @@ function stuff() {
     makeprojectile();
   }
 }
+
 function gravity(obj = Square) {
   if (obj.Y > floorY - obj.size/2) {
     obj.grounded = true;
-    if (obj.Y > floorY - obj.size/2 + 1) {squareup(1)}
+    if (obj.Y > floorY - obj.size/2 + 1) {objUp(obj, 1)}
   }
   else {obj.grounded = false}
   
@@ -126,13 +184,24 @@ function gravity(obj = Square) {
     if (obj.Yvel > 0) {obj.Yvel = 0}
   }
   
+  if (obj == Square) {squaredown(Square.Yvel*Square.speed/50)}
+  else {
+    obj.Y += obj.Yvel*Square.speed/50; //moves object down by scale of the page
+    obj.elem.style.top = obj.Y + "px";
+  }
 }
 
-function squaretomouse() { // doesn't work
-  Square.X = window.event.clientX;
-  Square.Y = window.event.clientY;
+function squaretomouse() {
+  Square.X = mouseX;
+  Square.Y = mouseY;
   square.style.left = Square.X + "px";
   square.style.top = Square.Y + "px";
+}
+
+function mousePosUpd(event) {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+  console.log(mouseX, mouseY)
 }
 
 function movement() { // there may be an easier way to do this
@@ -150,9 +219,11 @@ function movement() { // there may be an easier way to do this
       squaredown(Square.speed);
     }
   }
+  /*
   if (mode) {
     squaredown(Square.Yvel*Square.speed/50);
   }
+  */
 }
 
 function jump() {
@@ -169,9 +240,15 @@ function removsquare() {
 }
 
 function removprojecs() {
-  const elmnts = document.getElementById("projectilecontainer").childNodes;
-  const len = elmnts.length;
+  const len = artillery.length
   for (let i = 0; i < len; i++) {
+    const removedElem = artillery.pop();
+    removedElem.elem.remove();
+  }
+  
+  const elmnts = document.getElementById("projectilecontainer").childNodes;
+  const len2 = elmnts.length;
+  for (let i = 0; i < len2; i++) {
     console.log(elmnts[0]);
     elmnts[0].remove();
     // turns out HTMLCollections and NodeLists automatically update when you add or remove elements
@@ -181,24 +258,51 @@ function removprojecs() {
   }
 }
 
-function makeprojectile() {
+function makeprojectile(gravity = false) {
   projectileID ++;
   const projectile = document.createElement("div");
-  projectile.className = "projectile";
+  if (gravity) {
+    projectile.className = "artillery";
+  } else {
+    projectile.className = "projectile";
+  }
   projectile.id = projectileID
   projectile.style.left = Square.X + "px";
   projectile.style.top = Square.Y + "px";
   document.getElementById("projectilecontainer").appendChild(projectile);
+  if (gravity) {
+    const sin_ang = Math.sin(Square.Cannon.angle * degToRad);
+    const cos_ang = Math.cos(Square.Cannon.angle * degToRad);
+    artillery.push({
+      id: projectileID,
+      elem: projectile,
+      X: Square.X,
+      Y: Square.Y,
+      Xvel: cos_ang * Square.Cannon.vel,
+      Yvel: sin_ang * Square.Cannon.vel,
+      grounded: false,
+      size: 5,
+    });
+  }
 }
 
 function projectilemove() {
   const projectiles = document.getElementsByClassName("projectile");
+  // for projectiles:
   const len = projectiles.length;
   for (let i = 0; i < len; i++) {
     const projectile = projectiles[i];
     //console.log(projectile.style.left.slice(0, -2));
-    projectile.style.left = (Number(projectile.style.left.slice(0,-2)) + 10) + "px";
-    //projectile.style.top = Number(projectile.style.top) + 10 + "px";
+    projectile.style.left = (Number(projectile.style.left.slice(0,-2)) + Square.speed) + "px";
+  }
+
+  // for artillery:
+  const len2 = artillery.length;
+  for (let i = 0; i < len2; i++) {
+    const projectile = artillery[i];
+    projectile.X += projectile.Xvel*Square.speed/50;
+    projectile.elem.style.left = projectile.X + "px";
+    gravity(projectile)
   }
 }
 
